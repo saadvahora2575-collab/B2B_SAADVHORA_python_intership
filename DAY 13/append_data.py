3ba932e31cee30bf563cbb16e1527e0ec524c40c
saadvahora2575-collab <saadvahora2575@gmail.com>
2026-06-23 15:13:10 +0530

    new

---
+++ DAY 13/append_data.py
@@ -0,0 +1,13 @@
+new_name = input("Enter another name: ")
+
+with open("user.txt", "a") as file:
+    file.write(f"Name: {new_name}\n")
+
+print("Data appended to user.txt successfully!")
+
+print("\nCurrent file contents:")
+try:
+    with open("user.txt", "r") as file:
+        print(file.read())
+except FileNotFoundError:
+    pass
